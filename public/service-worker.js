7d93438b32db3f7ab857753550e0b00b0f42c935
JuVoros <Julianvoros@hotmail.com>
2021-11-17 15:18:42 -0800

    deploy to heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,19 @@
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/styles.css",
+  "/favicon.ico",
+  "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cached-v1";
-const FILES_TO_CACHE = ["/", "/index.html", "/index.js", "/styles.css", "/favicon.ico", "/manifest.webmanifest","/icons/icon-192x192.png","/icons/icon-512x512.png"];
 
-slef.addEventListener("install", function (evt) {
+self.addEventListener("install", function (evt) {
   evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
@@ -29,30 +38,33 @@ self.addEventListener("activate", function (evt) {
   self.clients.claim();
 });
 
-self.addEventListener("fetch", function(evt) {
-    if (evt.request.url.includes("/api/")) {
-      evt.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
           return fetch(evt.request)
-            .then(response => {
+            .then((response) => {
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
-  
+
               return response;
             })
-            .catch(err => {
+            .catch((err) => {
               return cache.match(evt.request);
             });
-        }).catch(err => console.log(err))
-      );
-  
-      return;
-    }
-  
-    evt.respondWith(
-        cache.match(evt.request).then(function(response) {
-          return response || fetch(evt.request);
         })
+        .catch((err) => console.log(err))
     );
+
+    return;
+  }
+
+  evt.respondWith(
+    cache.match(evt.request).then(function (response) {
+      return response || fetch(evt.request);
+    })
+  );
 });
