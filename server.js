35173ebbe979e5329c84eff4fff1a89e960bf0cd
JuVoros <Julianvoros@hotmail.com>
2021-11-17 14:27:43 -0800

    all code put in, now trying to deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require('dotenv').config();
 
 const PORT = 3000;
 
@@ -15,14 +16,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGOB_URI  ||"mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App doin taxes on port ${PORT}!`);
 });
