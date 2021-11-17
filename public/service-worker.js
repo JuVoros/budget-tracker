8f95d92abcd7f93b5537329d9bab13857c7e1df6
JuVoros <Julianvoros@hotmail.com>
2021-11-17 15:39:08 -0800

    deploy to heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,23 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cached-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/index.js",
   "/styles.css",
-  "/favicon.ico",
+  "/db.js",
+  "/index.js",
   "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cached-v1";
 
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+  );
+  evt.waitUntil(
+    cache.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 
   self.skipWaiting();
@@ -63,8 +65,10 @@ self.addEventListener("fetch", function (evt) {
   }
 
   evt.respondWith(
-    cache.match(evt.request).then(function (response) {
-      return response || fetch(evt.request);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      });
     })
   );
 });
