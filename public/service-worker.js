2a476993953abf31acafccaca74104397ec39471
JuVoros <Julianvoros@hotmail.com>
2021-11-17 19:56:00 -0800

    deploy to heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,10 +3,8 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/index.js",
   "/styles.css",
   "/db.js",
-  "/index.js",
   "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
